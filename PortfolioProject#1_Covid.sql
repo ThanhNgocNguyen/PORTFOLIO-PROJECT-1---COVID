aa3be16c4507a4e298c15d5b26c644187c8af5fb
Jade <106645819+ThanhNgocNguyen@users.noreply.github.com>
2022-06-04 20:29:54 +0700

    Update PortfolioProject#1_Covid.sql

--- PortfolioProject#1_Covid.sql
+++ PortfolioProject#1_Covid.sql
@@ -1,6 +1,6 @@
-COVID 19 DATA EXPLORATION (24/2/2020 - 30/4/2021) (Done thanks to the instructions from AlexTheAnalyst)
+--COVID 19 DATA EXPLORATION (24/2/2020 - 30/4/2021) (Done thanks to the instructions from AlexTheAnalyst)
 
-Skills used: Joins, CTE, Temp Tables, Window Functions, Aggregate Functions, Converting Data Types, Creating Views
+--Skills used: Joins, CTE, Temp Tables, Window Functions, Aggregate Functions, Converting Data Types, Creating Views
 
 
 SELECT *
